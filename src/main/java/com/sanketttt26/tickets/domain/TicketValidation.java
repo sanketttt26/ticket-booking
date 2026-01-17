2ac7d92ca8ea64718fe16f0205d410805b90ecb0
Sanket Pawar <sanketpawarsp7781@gmail.com>
2026-01-17 11:29:12 +0530

    add ticketvalidation entity;map relationships with ticket

--- src/main/java/com/sanketttt26/tickets/domain/TicketValidation.java
+++ src/main/java/com/sanketttt26/tickets/domain/TicketValidation.java
@@ -6,39 +6,32 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Entity
-@Table(name = "ticket")
+@Table(name = "ticket_validation")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Ticket {
+public class TicketValidation {
     @Id
-    @Column(name = "id", nullable = false, updatable = false)
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false, updatable = false)
     private UUID id;
 
     @Column(name="status",nullable = false)
     @Enumerated(EnumType.STRING)
-    private TicketStatusEnum status;
+    private TicketValidationStatusEnum status;
+
+    @Column(name="validation_method",nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TicketValidationMethodEnum validationMethod;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "ticket_type_id", nullable = false)
-    private TicketType type;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "buyer_id")
-    private User buyer;
-
-    @OneToMany(mappedBy = "ticket",cascade = CascadeType.ALL)
-    private List<TicketValidation> validations = new ArrayList<>();
-
-    //TODO QR Code or Barcode field can be added later
+    @JoinColumn(name = "ticket_id")
+    private Ticket ticket;
 
     @CreatedDate
     @Column(name="created_at",nullable = false,updatable = false)
